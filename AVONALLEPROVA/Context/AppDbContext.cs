ee6d28b0e83e89a384256e56fd652f381bd3d650
Pichau <Pichau@DESKTOP-3KCIM55>
2022-05-29 14:40:21 -0300

    Adicionar arquivos de projeto.

---
+++ AVONALLEPROVA/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AVONALLEPROVA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AVONALLEPROVA.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+        { }
+        public DbSet<Produto> Produtos { get; set; }//Serve para mapear a entidade criada, em uma tabela do banco de dados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ AVONALLEPROVA/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace AVONALLEPROVA.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public decimal price { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
